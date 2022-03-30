30fb6375f9344d321721d303601fd024dd649196
giorgios17 <giosur@live.it>
2022-03-30 15:35:43 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -9,8 +9,18 @@ Milestone 2
 ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
 messaggi relativi al contatto attivo all’interno del pannello della conversazione
 ● Click sul contatto mostra la conversazione del contatto cliccato
-*/
 
+Milestone 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+“mar” rimangono solo Marco e Martina)
+*/
 const app = new Vue({
     el: '#root',
     data: {
@@ -182,7 +192,7 @@ const app = new Vue({
             avatar: 'img/avatar_io.jpg'
         },
         activeContact: 0,
-
+        newMessage: ''
     },
     methods: {
         getActiveContact(indexActive) {
@@ -191,7 +201,7 @@ const app = new Vue({
         },
         profilePic(contact) {
             return `img/avatar${contact.avatar}.jpg`
-        }
+        },
 
     }
 })
